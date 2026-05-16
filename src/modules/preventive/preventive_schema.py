91a7c3de552334b74922759cf2a84a5ce823e793
Azathoth <imperialbenjie393@gmail.com>
2026-05-16 16:50:53 +0800

    feat: model for preventive action

---
+++ src/modules/preventive/preventive_schema.py
@@ -0,0 +1,13 @@
+from dataclasses import Field
+from pydantic import BaseModel
+
+class RiskPredictionRequest(BaseModel):
+    patient_id: str
+
+
+class RiskPredictionResponse(BaseModel):
+  status: str | None = Field(..., examples=["success", "failed"])
+  risk_score: int | None = 1
+  disease_progression: str | None = None
+  recommended_action:  str | None = None
+  
